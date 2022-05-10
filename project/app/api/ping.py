dc1c16da47295783946aa5960edf6ab46c40601f
instructor <thesavielli@gmail.com>
2022-05-10 17:31:46 +0600

    maybe work or not

--- project/app/api/ping.py
+++ project/app/api/ping.py
@@ -16,3 +16,11 @@ async def pong(settings: Settings = Depends(get_settings)):
         "environment": settings.environment,
         "testing": settings.testing
     }
+
+@router.get("/ping")
+async def pong(settings: Settings = Depends(get_settings)):
+    return {
+        "ping": "pong",
+        "environment": settings.environment,
+        "testing": settings.testing,
+    }
